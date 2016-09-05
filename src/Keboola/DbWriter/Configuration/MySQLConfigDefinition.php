c8dfaf6fc5866cd4c82cb7f55e9e05bc7598cf12
Erik Zigo <erik.zigo@keboola.com>
2016-09-05 15:00:33 +0200

    fix(config): custom definiton for encrypted connection

---
+++ src/Keboola/DbWriter/Configuration/MySQLConfigDefinition.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * @package ex-db-mysql
+ * @author Erik Zigo <erik.zigo@keboola.com>
+ */
+namespace Keboola\DbWriter\Configuration;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class MySQLConfigDefinition extends ConfigDefinition
+{
+    /**
+     * Generates the configuration tree builder.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('parameters');
+
+        $rootNode
+            ->children()
+                ->scalarNode('data_dir')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+                ->scalarNode('writer_class')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+                ->arrayNode('db')
+                    ->children()
+                        ->scalarNode('host')->end()
+                        ->scalarNode('port')->end()
+                        ->scalarNode('database')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('user')
+                            ->isRequired()
+                        ->end()
+                        ->scalarNode('password')->end()
+                        ->scalarNode('#password')->end()
+                        ->append($this->addSshNode())
+						->append($this->addSslNode())
+                    ->end()
+                ->end()
+                ->arrayNode('tables')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('tableId')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->scalarNode('dbName')
+                                ->isRequired()
+                                ->cannotBeEmpty()
+                            ->end()
+                            ->booleanNode('incremental')
+                                ->defaultValue(false)
+                            ->end()
+                            ->booleanNode('export')
+                                ->defaultValue(true)
+                            ->end()
+                            ->arrayNode('primaryKey')
+                                ->prototype('scalar')
+                                ->end()
+                            ->end()
+                            ->arrayNode('items')
+                                ->prototype('array')
+                                    ->children()
+                                        ->scalarNode('name')
+                                            ->isRequired()
+                                            ->cannotBeEmpty()
+                                        ->end()
+                                        ->scalarNode('dbName')
+                                            ->isRequired()
+                                            ->cannotBeEmpty()
+                                        ->end()
+                                        ->scalarNode('type')
+                                            ->isRequired()
+                                            ->cannotBeEmpty()
+                                        ->end()
+                                        ->scalarNode('size')
+                                        ->end()
+                                        ->scalarNode('nullable')
+                                        ->end()
+                                        ->scalarNode('default')
+                                        ->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+
+    public function addSshNode()
+    {
+        $builder = new TreeBuilder();
+        $node = $builder->root('ssh');
+
+        $node
+            ->children()
+                ->booleanNode('enabled')->end()
+                ->arrayNode('keys')
+                    ->children()
+                        ->scalarNode('private')->end()
+                        ->scalarNode('#private')->end()
+                        ->scalarNode('public')->end()
+                    ->end()
+                ->end()
+                ->scalarNode('sshHost')->end()
+                ->scalarNode('sshPort')->end()
+                ->scalarNode('remoteHost')->end()
+                ->scalarNode('remotePort')->end()
+                ->scalarNode('localPort')->end()
+                ->scalarNode('user')->end()
+            ->end()
+        ;
+
+        return $node;
+    }
+
+	public function addSslNode()
+	{
+		$builder = new TreeBuilder();
+		$node = $builder->root('ssl');
+
+		$node
+			->children()
+				->booleanNode('enabled')->end()
+				->scalarNode('ca')->end()
+				->scalarNode('cert')->end()
+				->scalarNode('key')->end()
+				->scalarNode('cipher')->end()
+			->end()
+		;
+
+		return $node;
+	}
+}
