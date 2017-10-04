ee4ce752fb07571a388564c74d7af8b463d51db4
Vladimír Kriška <vlado@keboola.com>
2017-10-04 20:53:47 +0200

    Log message when falling back to utf8

--- src/Keboola/DbWriter/Writer/MySQL.php
+++ src/Keboola/DbWriter/Writer/MySQL.php
@@ -114,6 +114,7 @@ class MySQL extends Writer implements WriterInterface
 			$pdo->exec("SET NAMES $this->charset;");
 		} catch (\PDOException $e) {
 			$this->charset = 'utf8';
+			echo 'Falling back to ' . $this->charset . ' charset' . "\n";
 			$pdo->exec("SET NAMES $this->charset;");
 		}
 
